979798a2d6d7c2cad6b8c52c5310ea615440039b
edoardope <pencoedoardo@gmail.com>
2023-03-28 10:57:38 +0200

    snack 1

---
+++ snack 1/main.js
@@ -0,0 +1,18 @@
+let n1 = prompt("Please enter first number")
+let n2 = prompt("Please enter second number")
+
+if (n1 > n2) {
+
+    console.log(`${n1}`)
+    document.write(`${n1}`);
+
+} else if (n2 > n1) {
+
+    console.log(`${n2}`)
+    document.write(`${n2}`);
+
+} else {
+
+    document.write(`${n1} e ${n2} sono uguali`);
+
+}
